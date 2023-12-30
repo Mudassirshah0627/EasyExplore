495d308017e8f31acbb05f29469dcbf4d82df7f2
mudassirshah1335 <mudassirshar1335@gmail.com>
2023-12-29 23:04:47 -0800

    All changes completed

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Gilgit Baltistan',
+        des: 'Discover the enchanting beauty of Gilgit-Baltistan, where jagged peaks and pristine lakes create a mesmerizing canvas for nature lovers.Explore the rich cultural tapestry, vibrant bazaars, and hospitable locals, making Gilgit-Baltistan a captivating destination for those seeking an unforgettable blend of adventure and tranquility.',
+        image: '/img/slider1.jpg'
+    },
+    {
+        name: 'Minar-e-Pakistan',
+        des: 'Minar-e-Pakistan in Lahore stands tall, commemorating the historic Lahore Resolution and offering visitors a powerful blend of architectural beauty and national significance.Minar-e-Pakistan, a towering symbol in Lahore, stands as a poignant tribute to the historic Lahore Resolution. Visitors can ascend its heights for panoramic views of the city, while the monuments significance as the birthplace of Pakistan adds a compelling layer to its architectural splendor.',
+        image: '/img/slider2.jpg'
+    },
+    {
+        name: 'Badshahi Mosque',
+        des: 'Badshahi Mosque in Lahore, a Mughal marvel, captivates with its grand architecture and rich history, making it a top tourist destination in Pakistan.Badshahi Mosque in Lahore, a masterpiece of Mughal architecture, enchants with its grandeur and intricate design. Steeped in history, its a must-visit for those seeking awe-inspiring beauty and cultural richness in Pakistan.',
+        image: '/img/slider3.jpg'
+    },
+    {
+        name: 'Thar Desert',
+        des: 'Discover the allure of Pakistans Thar Desert, a golden expanse inviting travelers to embrace camel safaris and experience the rich cultural tapestry of this arid wonder.Explore the enchanting vastness of the Thar Desert in Pakistan, where golden sands stretch as far as the eye can see, offering a mesmerizing backdrop for camel safaris and glimpses into the unique desert lifestyle.',
+        image: '/img/slider4.jpg'
+    },
+    {
+        name: 'Clifton Beach',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: '/img/slider5.jpg'
+    }
+]
